9d44220bebed9397d3d9db4a337e38787439d622
ricardocaiuba <ricardo.caiuba@gmail.com>
2020-05-11 18:25:25 -0300

    Implementação reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import promise from "redux-promise";
 
 import "./index.scss";
 import App from "./App";
 
 //import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+import reducers from "./services/reducers";
+
+const store = applyMiddleware(promise)(createStore)(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
